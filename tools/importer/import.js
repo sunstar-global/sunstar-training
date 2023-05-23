8db9ea2534e632a9bfc45904fd5791e16ae00d6a
Satya Deep Maheshwari <satyam@adobe.com>
2023-05-23 14:09:17 +0530

    Fix indentation

--- tools/importer/import.js
+++ tools/importer/import.js
@@ -14,71 +14,71 @@
 
 
 const createMetadata = (main, document) => {
-    const meta = {};
+  const meta = {};
 
-    const title = document.querySelector('title');
-    if (title) {
-        meta.Title = title.innerHTML.replace(/[\n\t]/gm, '');
-    }
+  const title = document.querySelector('title');
+  if (title) {
+    meta.Title = title.innerHTML.replace(/[\n\t]/gm, '');
+  }
 
-    const desc = document.querySelector('[property="og:description"]');
-    if (desc) {
-        meta.Description = desc.content;
-    }
+  const desc = document.querySelector('[property="og:description"]');
+  if (desc) {
+    meta.Description = desc.content;
+  }
 
-    const img = document.querySelector('[property="og:image"]');
-    if (img && img.content) {
-        const el = document.createElement('img');
-        el.src = img.content;
-        meta.Image = el;
-    }
+  const img = document.querySelector('[property="og:image"]');
+  if (img && img.content) {
+    const el = document.createElement('img');
+    el.src = img.content;
+    meta.Image = el;
+  }
 
-    const block = WebImporter.Blocks.getMetadataBlock(document, meta);
-    main.append(block);
+  const block = WebImporter.Blocks.getMetadataBlock(document, meta);
+  main.append(block);
 
-    return meta;
+  return meta;
 };
 
 export default {
-    /**
-     * Apply DOM operations to the provided document and return
-     * the root element to be then transformed to Markdown.
-     * @param {HTMLDocument} document The document
-     * @param {string} url The url of the page imported
-     * @param {string} html The raw html (the document is cleaned up during preprocessing)
-     * @param {object} params Object containing some parameters given by the import process.
-     * @returns {HTMLElement} The root element to be transformed
-     */
-    transformDOM: ({
-        // eslint-disable-next-line no-unused-vars
-        document, url, html, params,
-    }) => {
-        // define the main element: the one that will be transformed to Markdown
-        const main = document.body;
+  /**
+   * Apply DOM operations to the provided document and return
+   * the root element to be then transformed to Markdown.
+   * @param {HTMLDocument} document The document
+   * @param {string} url The url of the page imported
+   * @param {string} html The raw html (the document is cleaned up during preprocessing)
+   * @param {object} params Object containing some parameters given by the import process.
+   * @returns {HTMLElement} The root element to be transformed
+   */
+  transformDOM: ({
+    // eslint-disable-next-line no-unused-vars
+    document, url, html, params,
+  }) => {
+    // define the main element: the one that will be transformed to Markdown
+    const main = document.body;
 
-        // use helper method to remove header, footer, etc.
-        WebImporter.DOMUtils.remove(main, [
-            'header',
-            'footer',
-        ]);
+    // use helper method to remove header, footer, etc.
+    WebImporter.DOMUtils.remove(main, [
+      'header',
+      'footer',
+    ]);
 
-        // create the metadata block and append it to the main element
-        createMetadata(main, document);
+    // create the metadata block and append it to the main element
+    createMetadata(main, document);
 
-        return main;
-    },
+    return main;
+  },
 
-    /**
-     * Return a path that describes the document being transformed (file name, nesting...).
-     * The path is then used to create the corresponding Word document.
-     * @param {HTMLDocument} document The document
-     * @param {string} url The url of the page imported
-     * @param {string} html The raw html (the document is cleaned up during preprocessing)
-     * @param {object} params Object containing some parameters given by the import process.
-     * @return {string} The path
-     */
-    generateDocumentPath: ({
-        // eslint-disable-next-line no-unused-vars
-        document, url, html, params,
-    }) => WebImporter.FileUtils.sanitizePath(new URL(url).pathname.replace(/\.html$/, '').replace(/\/$/, '')),
+  /**
+   * Return a path that describes the document being transformed (file name, nesting...).
+   * The path is then used to create the corresponding Word document.
+   * @param {HTMLDocument} document The document
+   * @param {string} url The url of the page imported
+   * @param {string} html The raw html (the document is cleaned up during preprocessing)
+   * @param {object} params Object containing some parameters given by the import process.
+   * @return {string} The path
+   */
+  generateDocumentPath: ({
+    // eslint-disable-next-line no-unused-vars
+    document, url, html, params,
+  }) => WebImporter.FileUtils.sanitizePath(new URL(url).pathname.replace(/\.html$/, '').replace(/\/$/, '')),
 };
