828246bfa1a37d012ba8fa77711cca015c5081ff
Piyush Jindal <jindaliiita@gmail.com>
2023-06-06 14:05:21 +0530

    issue-2 Footer Changes (#78)
    * issue-77 footer small screen
    
    * issue-77 Review Changes
    
    * issue-77 Incorporating Review Changes
    
    * issue-77 Footer Large Screen
    
    * issue-77 Minor Change
    
    * issue-77 Minor Change
    
    * issue-77 Footer path changes
    
    ---------
    
    Co-authored-by: piyushjindal <piyushjindal@adobe.com>

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,4 +1,49 @@
-import { readBlockConfig, decorateIcons } from '../../scripts/lib-franklin.js';
+import {
+  readBlockConfig,
+  decorateButtons,
+  decorateSections,
+  updateSectionsStatus,
+} from '../../scripts/lib-franklin.js';
+
+function wrapSocialAndNavLinks(block) {
+  const socialNavWrapper = document.createElement('div');
+  socialNavWrapper.classList.add('social-nav');
+  const navLinks = block.querySelector('.nav-links');
+  const social = block.querySelector('.social');
+
+  socialNavWrapper.appendChild(navLinks);
+  socialNavWrapper.appendChild(social);
+
+  block.querySelector('.primary').after(socialNavWrapper);
+}
+
+function decorateFooter(block) {
+  wrapSocialAndNavLinks(block);
+
+  const primaryFooter = block.querySelector('.section.primary>div');
+  const itemsList = [];
+  const childrens = [...primaryFooter.children];
+  let index = 0;
+
+  while (index < childrens.length) {
+    const navItem = document.createElement('div');
+    navItem.classList.add('primary-items');
+    navItem.appendChild(childrens[index]);
+    if (childrens[index + 1] && childrens[index + 1].tagName === 'UL') {
+      navItem.appendChild(childrens[index + 1]);
+      index += 2;
+    } else {
+      index += 1;
+    }
+
+    itemsList.push(navItem);
+  }
+
+  primaryFooter.innerHTML = '';
+  itemsList.forEach((item) => {
+    primaryFooter.appendChild(item);
+  });
+}
 
 /**
  * loads and decorates the footer
@@ -18,8 +63,11 @@ export default async function decorate(block) {
     // decorate footer DOM
     const footer = document.createElement('div');
     footer.innerHTML = html;
+    await decorateSections(footer);
+    updateSectionsStatus(footer);
 
-    decorateIcons(footer);
     block.append(footer);
+    decorateButtons(block);
+    decorateFooter(block);
   }
 }
