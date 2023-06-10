25e24f9ee9aac5218df24831a94cf3dd841b98d8
actinium15 <actinium15@gmail.com>
2023-06-10 16:29:58 +0530

    fix column import (#98)

--- tools/importer/import.js
+++ tools/importer/import.js
@@ -162,15 +162,22 @@ function convertBackgroundImgsToForegroundImgs(sourceNode, targetNode = sourceNo
 function createColumnBlockFromSection(document) {
   document.querySelectorAll('div.section-container').forEach((section) => {
     const block = [['Columns']];
-    // create a column block from the section
-    // but only if it contains two columns and isn't a hero section
+    /* create a column block from the section
+       but only if it contains
+       * two columns
+       * isn't a hero section
+       * doesn't have an embed
+     */
     const heroParent = Array.from(section.parentElement.classList)
       .filter((s) => /hero/.test(s)).length;
+    const hasEmbed = !!section.querySelector('.wp-block-embed');
     const contentColumns = Array.from(section.children)
       .filter(
-        (el) => (el.tagName === 'DIV' || el.tagName === 'FIGURE' || el.tagName === 'IMG'),
+        (el) => (el.tagName === 'DIV'
+          || el.tagName === 'FIGURE'
+          || el.tagName === 'IMG'),
       );
-    if (!heroParent && contentColumns
+    if (!heroParent && !hasEmbed && contentColumns
       && contentColumns.length === 2
       && section.children.length === 2
       && section.querySelectorAll('p').length !== 0) {
