6839425fba1d6243f10516607c1f8d3a0d246a86
JiangLong2019 <57093988+JiangLong2019@users.noreply.github.com>
2023-06-12 21:21:44 +0900

    Fix lint js failings

--- blocks/table/table.js
+++ blocks/table/table.js
@@ -1,12 +1,13 @@
 export default async function decorate(block) {
-  var tableDivHtml = '';
+  let tableDivHtml = '';
   [...block.children].forEach((child, i) => {
     [...child.children].forEach((childDiv) => {
       if (!i) childDiv.classList.add('table-head');
       tableDivHtml += childDiv.outerHTML;
     });
-    if (block.getAttribute("style") === null) {
-      block.setAttribute("style", "--col:" + [...child.children].length);
+    if (block.getAttribute('style') === null) {
+      const colLength = [...child.children].length.toString();
+      block.setAttribute('style', `--col:${colLength}`);
     }
   });
   block.innerHTML = '';
