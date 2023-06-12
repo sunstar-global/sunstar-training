e961e86852998d4695922b8ae96c5687dec3338d
JiangLong2019 <57093988+JiangLong2019@users.noreply.github.com>
2023-06-12 10:54:30 +0900

    Refactor table block
    1. Merge table block and table responsive block to one block by block options
    2. Replace HTML table by flex.

--- blocks/table/table.js
+++ blocks/table/table.js
@@ -1,5 +1,14 @@
-import { buildTable } from '../table-responsive/table-responsive.js';
-
 export default async function decorate(block) {
-  buildTable(block);
+  var tableDivHtml = '';
+  [...block.children].forEach((child, i) => {
+    [...child.children].forEach((childDiv) => {
+      if (!i) childDiv.classList.add('table-head');
+      tableDivHtml += childDiv.outerHTML;
+    });
+    if (block.getAttribute("style") === null) {
+      block.setAttribute("style", "--col:" + [...child.children].length);
+    }
+  });
+  block.innerHTML = '';
+  block.innerHTML = tableDivHtml;
 }
