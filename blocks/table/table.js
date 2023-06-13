c3ee50924504fbd63798fc54aa0a8e6acfaef8e0
JiangLong2019 <57093988+JiangLong2019@users.noreply.github.com>
2023-06-13 16:23:00 +0900

    Few updates by PR review
    1. Change CSS to mobile first(min-width)
    2. Handle multiple rows table
    3. Refactored border color

--- blocks/table/table.js
+++ blocks/table/table.js
@@ -1,13 +1,19 @@
 export default async function decorate(block) {
   let tableDivHtml = '';
   [...block.children].forEach((child, i) => {
+    if (block.getAttribute('style') === null) {
+      const rowLength = ([...block.children].length).toString();
+      const rowLengthMinusOne = ([...block.children].length - 1).toString();
+      block.setAttribute('style', `--row:${rowLength};--rowminus:${rowLengthMinusOne}`);
+    }
     [...child.children].forEach((childDiv) => {
       if (!i) childDiv.classList.add('table-head');
       tableDivHtml += childDiv.outerHTML;
     });
-    if (block.getAttribute('style') === null) {
+    if (block.getAttribute('style') !== null && !block.getAttribute('style').includes('--col')) {
       const colLength = [...child.children].length.toString();
-      block.setAttribute('style', `--col:${colLength}`);
+      const rowStyles = block.getAttribute('style');
+      block.setAttribute('style', `${rowStyles};--col:${colLength}`);
     }
   });
   block.innerHTML = '';
