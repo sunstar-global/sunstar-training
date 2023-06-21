fa67cb5f4ef9f164cc97010df6f0f91e84198da5
David Bosschaert <david.bosschaert@gmail.com>
2023-06-21 12:36:45 +0100

    Add paging to search (#120)

--- blocks/search-results/search-results.js
+++ blocks/search-results/search-results.js
@@ -1,8 +1,16 @@
-import { getSearchWidget } from '../../scripts/scripts.js';
+import { fetchIndex, getSearchWidget } from '../../scripts/scripts.js';
 
-function getSearchParams() {
-  const searchTerm = new URLSearchParams(window.location.search).get('s');
-  return searchTerm;
+export function getSearchParams(searchParams) {
+  let curPage = new URLSearchParams(searchParams).get('pg');
+  if (!curPage) {
+    curPage = 0;
+  } else {
+    // convert the current page to a number
+    curPage = parseInt(curPage, 10);
+  }
+
+  const searchTerm = new URLSearchParams(searchParams).get('s');
+  return { searchTerm, curPage };
 }
 
 /**
@@ -24,9 +32,85 @@ function setResultValue(el, value, term) {
   el.innerHTML = txtHTML.replaceAll(regex, '<strong>$1</strong>');
 }
 
-async function searchPages(term) {
-  const resp = await fetch(`${window.location.origin}/query-index.json`);
-  const json = await resp.json();
+/**
+ * Add a paging widget to the div. The paging widget looks like this:
+ * <pre><code>
+ * &lt; 1 2 3 &gt;
+ * </code></pre>
+ * The numbers are hyperlinks to the repective pages and the &lt; and &gt;
+ * buttons are links to next and previous pages. If this is the first page
+ * then the &lt; link has the style 'disabled' and if this is the lase one
+ * the &gt; link is disabled.
+ * @param {HTMLElement} div - The div to add the widget to
+ * @param {number} curpage - The current page number (starting at 0)
+ * @param {number} totalPages - The total number of pages
+ * @param {Document} doc - The current Document
+ * @param {Location} curLocation - THe current window.location to use
+ */
+export function addPagingWidget(
+  div,
+  curpage,
+  totalPages,
+  doc = document,
+  curLocation = window.location,
+) {
+  const queryParams = new URLSearchParams(curLocation.search);
+
+  const nav = doc.createElement('ul');
+  nav.classList.add('pagination');
+
+  const lt = doc.createElement('li');
+  lt.classList.add('page');
+  lt.classList.add('prev');
+  const lta = doc.createElement('a');
+  if (curpage === 0) {
+    lt.classList.add('disabled');
+  } else {
+    queryParams.set('pg', curpage - 1);
+    lta.href = `${curLocation.pathname}?${queryParams}`;
+  }
+  lt.appendChild(lta);
+
+  nav.appendChild(lt);
+
+  for (let i = 0; i < totalPages; i += 1) {
+    const numli = doc.createElement('li');
+    if (i === curpage) {
+      numli.classList.add('active');
+    }
+
+    const a = doc.createElement('a');
+    a.innerText = i + 1;
+
+    queryParams.set('pg', i);
+    a.href = `${curLocation.pathname}?${queryParams}`;
+    numli.appendChild(a);
+
+    nav.appendChild(numli);
+  }
+
+  const rt = doc.createElement('li');
+  rt.classList.add('page');
+  rt.classList.add('next');
+  const rta = doc.createElement('a');
+  if (curpage === totalPages - 1) {
+    rt.classList.add('disabled');
+  } else {
+    queryParams.set('pg', curpage + 1);
+    rta.href = `${curLocation.pathname}?${queryParams}`;
+  }
+  rt.appendChild(rta);
+
+  nav.appendChild(rt);
+
+  div.appendChild(nav);
+}
+
+async function searchPages(term, page) {
+  const json = await fetchIndex('query-index');
+
+  const resultsPerPage = 10;
+  const startResult = page * resultsPerPage;
 
   const result = json.data
     .filter((entry) => `${entry.description} ${entry.title}`.toLowerCase()
@@ -39,9 +123,9 @@ async function searchPages(term) {
   summary.innerHTML = `${result.length} result${result.length === 1 ? '' : 's'} found for "<strong>${term}</strong>"`;
   div.appendChild(summary);
 
-  const firstPage = result.slice(0, 10);
+  const curPage = result.slice(startResult, startResult + resultsPerPage);
 
-  firstPage.forEach((line) => {
+  curPage.forEach((line) => {
     const res = document.createElement('div');
     res.classList.add('search-result');
     const header = document.createElement('h3');
@@ -81,6 +165,11 @@ async function searchPages(term) {
     div.appendChild(res);
   });
 
+  const totalResults = result.length;
+  const totalPages = Math.ceil(totalResults / resultsPerPage);
+
+  addPagingWidget(div, page, totalPages);
+
   return div.children;
 }
 
@@ -88,14 +177,14 @@ async function searchPages(term) {
  * decorates the header, mainly the nav
  * @param {Element} block The header block element
  */
-export default async function decorate(block) {
-  const searchTerm = getSearchParams();
+export default async function decorate(block, curLocation = window.location) {
+  const { searchTerm, curPage } = getSearchParams(curLocation.search);
 
   block.innerHTML = '';
   block.append(getSearchWidget(searchTerm, true));
 
   if (searchTerm) {
-    const results = await searchPages(searchTerm);
+    const results = await searchPages(searchTerm, curPage);
     block.append(...results);
   }
 }
---
+++ test/blocks/search-results/search-results.test.js
@@ -0,0 +1,224 @@
+/* eslint-disable no-unused-expressions */
+/* global describe before it */
+
+import { expect } from '@esm-bundle/chai';
+import { readFile } from '@web/test-runner-commands';
+import sinon from 'sinon';
+
+const scripts = {};
+
+document.write(await readFile({ path: './search-results.plain.html' }));
+
+function mockElement(name) {
+  const el = {
+    tagName: name,
+    classList: new Set(),
+    children: [],
+  };
+  el.appendChild = (c) => {
+    el.children.push(c);
+  };
+  return el;
+}
+
+describe('Search Results', () => {
+  before(async () => {
+    const mod = await import('../../../blocks/search-results/search-results.js');
+    Object
+      .keys(mod)
+      .forEach((func) => {
+        scripts[func] = mod[func];
+      });
+  });
+
+  it('Get Search Params', () => {
+    const res = scripts.getSearchParams('?s=e+a&pg=1');
+    expect(res.searchTerm).to.equal('e a');
+    expect(res.curPage).to.equal(1);
+  });
+
+  it('Get Search Params Empty', () => {
+    const res = scripts.getSearchParams('?');
+    expect(res.curPage).to.equal(0);
+    expect(res.searchTerm).to.be.null;
+  });
+
+  it('Paging widget 1', () => {
+    const parentDiv = mockElement('div');
+    const doc = {
+      createElement: mockElement,
+    };
+    const loc = {
+      search: '?lang=de',
+      pathname: '/search',
+    };
+
+    scripts.addPagingWidget(parentDiv, 0, 2, doc, loc);
+
+    const navEl = parentDiv.children[0];
+    expect(navEl.tagName).to.equal('ul');
+    expect(navEl.classList).to.deep.equal(new Set(['pagination']));
+
+    const prevPg = navEl.children[0];
+    expect(prevPg.tagName).to.equals('li');
+    expect(prevPg.classList).to.deep.equal(new Set(['page', 'prev', 'disabled']));
+    const prevPgA = prevPg.children[0];
+    expect(prevPgA.tagName).to.equal('a');
+    expect(prevPgA.href).to.be.undefined;
+
+    const pg1 = navEl.children[1];
+    expect(pg1.tagName).to.equal('li');
+    expect(pg1.classList).to.deep.equal(new Set(['active']));
+    const pg1a = pg1.children[0];
+    expect(pg1a.innerText).to.equal(1);
+    expect(pg1a.href).to.equal('/search?lang=de&pg=0');
+
+    const pg2 = navEl.children[2];
+    expect(pg2.tagName).to.equal('li');
+    const pg2a = pg2.children[0];
+    expect(pg2a.innerText).to.equal(2);
+    expect(pg2a.href).to.equal('/search?lang=de&pg=1');
+
+    const nextPg = navEl.children[3];
+    expect(nextPg.tagName).to.equal('li');
+    expect(nextPg.classList).to.deep.equal(new Set(['page', 'next']));
+    const nextPgA = nextPg.children[0];
+    expect(nextPgA.tagName).to.equal('a');
+    expect(nextPgA.href).to.equal('/search?lang=de&pg=1');
+  });
+
+  it('Paging widget 2', () => {
+    const parentDiv = mockElement('div');
+    const doc = {
+      createElement: mockElement,
+    };
+    const loc = {
+      search: '?s=xyz&pg=1',
+      pathname: '/search',
+    };
+
+    scripts.addPagingWidget(parentDiv, 1, 3, doc, loc);
+
+    const navEl = parentDiv.children[0];
+    expect(navEl.tagName).to.equal('ul');
+    expect(navEl.classList).to.deep.equal(new Set(['pagination']));
+
+    const prevPg = navEl.children[0];
+    expect(prevPg.tagName).to.equals('li');
+    expect(prevPg.classList).to.deep.equal(new Set(['page', 'prev']));
+    const prevPgA = prevPg.children[0];
+    expect(prevPgA.tagName).to.equal('a');
+    expect(prevPgA.href).to.equal('/search?s=xyz&pg=0');
+
+    const pg1 = navEl.children[1];
+    expect(pg1.classList).to.not.include('active');
+    const pg2 = navEl.children[2];
+    expect(pg2.classList).to.include('active');
+    const pg3 = navEl.children[3];
+    expect(pg3.classList).to.not.include('active');
+
+    const nextPg = navEl.children[4];
+    expect(nextPg.classList).to.deep.equal(new Set(['page', 'next']));
+    const nextPgA = nextPg.children[0];
+    expect(nextPgA.href).to.equal('/search?s=xyz&pg=2');
+  });
+
+  it('Paging widget 3', () => {
+    const parentDiv = mockElement('div');
+    const doc = {
+      createElement: mockElement,
+    };
+    const loc = {
+      search: '?s=xyz&pg=999&lang=fr',
+      pathname: '/search',
+    };
+
+    scripts.addPagingWidget(parentDiv, 0, 1, doc, loc);
+
+    const navEl = parentDiv.children[0];
+    expect(navEl.tagName).to.equal('ul');
+    expect(navEl.classList).to.deep.equal(new Set(['pagination']));
+
+    const prevPg = navEl.children[0];
+    expect(prevPg.tagName).to.equals('li');
+    expect(prevPg.classList).to.deep.equal(new Set(['page', 'prev', 'disabled']));
+    const prevPgA = prevPg.children[0];
+    expect(prevPgA.tagName).to.equal('a');
+    expect(prevPgA.href).to.be.undefined;
+
+    const pg1 = navEl.children[1];
+    expect(pg1.classList).to.include('active');
+
+    const nextPg = navEl.children[2];
+    expect(nextPg.tagName).to.equals('li');
+    expect(nextPg.classList).to.deep.equal(new Set(['page', 'next', 'disabled']));
+    const nextPgA = prevPg.children[0];
+    expect(nextPgA.tagName).to.equal('a');
+    expect(nextPgA.href).to.be.undefined;
+  });
+
+  it('Page generation', async () => {
+    const queryIndex = '/query-index.json';
+    const mf = sinon.stub(window, 'fetch');
+    mf.callsFake((v) => {
+      if (v.startsWith(queryIndex)) {
+        return {
+          ok: true,
+          json: () => ({
+            data: [
+              { path: '/news/a/', title: 'a text', lastModified: 1685443971 },
+              { path: '/news/b/', title: 'some b', lastModified: 1685443972 },
+              { path: '/news/c/', title: 'c text', lastModified: 1685443973 },
+            ],
+          }),
+        };
+      }
+
+      return {
+        ok: false, json: () => ({ data: [] }), text: () => '',
+      };
+    });
+
+    const block = document.querySelector('.search-results');
+    const loc = {
+      search: '?s=tex',
+      pathname: '/search',
+    };
+
+    try {
+      await scripts.default(block, loc);
+    } finally {
+      mf.restore();
+    }
+
+    const searchForm = block.children[0];
+    expect(searchForm.nodeName).to.equal('FORM');
+    expect(searchForm.classList.toString()).to.equal('search');
+
+    const searchSummary = block.children[1];
+    expect(searchSummary.nodeName).to.equal('H3');
+    expect(searchSummary.classList.toString()).to.equal('search-summary');
+    expect(searchSummary.innerHTML).to.equal('2 results found for "<strong>tex</strong>"');
+
+    const res1 = block.children[2];
+    expect(res1.nodeName).to.equal('DIV');
+    expect(res1.classList.toString()).to.equal('search-result');
+    const res1h3 = res1.children[0];
+    expect(res1h3.nodeName).to.equal('H3');
+    const res1h3a = res1h3.children[0];
+    expect(res1h3a.nodeName).to.equal('A');
+    expect(res1h3a.href.endsWith('/news/a/')).to.be.true;
+
+    const res2 = block.children[3];
+    expect(res2.nodeName).to.equal('DIV');
+    expect(res2.classList.toString()).to.equal('search-result');
+    const res2h3 = res2.children[0];
+    expect(res2h3.nodeName).to.equal('H3');
+    const res2h3a = res2h3.children[0];
+    expect(res2h3a.nodeName).to.equal('A');
+    expect(res2h3a.href.endsWith('/news/c/')).to.be.true;
+
+    const pageWidget = block.children[4];
+    expect(pageWidget.className.toString()).to.equal('pagination');
+  });
+});
