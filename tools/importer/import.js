032430671224f024b03af61c23e562f936c4bf99
Lucian Felix Ghita <lucianfelix@users.noreply.github.com>
2023-07-27 17:17:28 +0200

    Import: transform relative links to absolute ones during import (#359)
    * Import: transform relative links to absolute ones during import
    
    * Remove trailing slash
    
    * Use page domain
    
    ---------
    
    Co-authored-by: Carsten Ziegeler <cziegeler@apache.org>

--- tools/importer/import.js
+++ tools/importer/import.js
@@ -496,10 +496,41 @@ function getFomattedDate(newsDate) {
   return `${year}${month}${day}`;
 }
 
+/**
+ * Prefixes relative links with the target domain
+ * @param {HTMLDocument} document The document
+ */
+function fixRelativeLinks(document) {
+  document.querySelectorAll('a').forEach((a) => {
+    const targetDomain = 'https://main--sunstar-engineering--hlxsites.hlx.page';
+    // if the link is relative, make it absolute
+    if (a.href.startsWith('/')) {
+      let link = a.href;
+      const p1 = a.href.indexOf('#');
+      const p2 = a.href.indexOf('?');
+      let p = p1;
+      if (p1 < 0 || (p2 > 0 && p2 < p1)) {
+        p = p2;
+      }
+      if (p > 0) {
+        link = a.href.substring(0, p);
+        if (link.endsWith('/')) {
+          link = link.substring(0, link.length - 1);
+        }
+        link += a.href.substring(p);
+      } else if (link.endsWith('/')) {
+        link = link.substring(0, link.length - 1);
+      }
+      a.href = targetDomain + link;
+    }
+  });
+}
+
 function customImportLogic(doc) {
   removeCookiesBanner(doc);
 
   addBreadCrumb(doc);
+  fixRelativeLinks(doc);
   addListBlock(doc);
   addCarouselItems(doc);
   addHeroHorizontalTabs(doc);
@@ -598,6 +629,7 @@ export default {
 
     const initialReplace = new URL(url).pathname.replace(/\.html$/, '').replace(/\/$/, '');
 
+    console.log(`pathname: ${pathname} -> initialReplace: ${initialReplace}`);
     // Custom handling for Japanese news pages
     if (pathname.toLowerCase().indexOf('/ja/news/') !== -1 && preProcessMetadata && preProcessMetadata.NewsDate) {
       return WebImporter.FileUtils.sanitizePath(initialReplace.replace(/\/ja\/news\/.*$/, `/ja/news/${getFomattedDate(preProcessMetadata.NewsDate)}}`));
