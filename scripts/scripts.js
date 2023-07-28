459fbb91645de11ed55cd1369e0a9c48f4217d67
Satya Deep Maheshwari <m.satyadeep@gmail.com>
2023-07-28 16:58:36 +0530

    Fix font load regression (#378)
    Co-authored-by: Satya Deep Maheshwari <satyam@adobe.com>

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -165,7 +165,7 @@ function decoratePageStyles() {
  * load fonts.css and set a session storage flag
  */
 async function loadFonts() {
-  await loadCSS(`${window.hlx.codeBasePath}/styles/fonts.css`);
+  await loadCSS(`${window.hlx.codeBasePath}/styles/fonts/fonts.css`);
   try {
     if (!window.location.hostname.includes('localhost')) sessionStorage.setItem('fonts-loaded', 'true');
   } catch (e) {
