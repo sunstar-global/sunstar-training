4e3a8d620f2e10204c44552fe9a0825dd86b02f5
Piyush Jindal <jindaliiita@gmail.com>
2023-08-21 09:23:09 +0530

    issue-460 Embed block changes (#515)
    * issue-460 Embed block changes
    
    * issue-460 Changes wrt to CLS
    
    ---------
    
    Co-authored-by: piyushjindal <piyushjindal@adobe.com>

--- blocks/embed/embed.js
+++ blocks/embed/embed.js
@@ -4,13 +4,10 @@
  * https://www.hlx.live/developer/block-collection/embed
  */
 
-const getDefaultEmbed = (url) => `<div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;">
-    <iframe src="${url.href}" style="border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;"
-      scrolling="no" allow="encrypted-media" title="Content from ${url.hostname}" loading="lazy">
-    </iframe>
-  </div>`;
+import { loadScript } from '../../scripts/scripts.js';
+import { loadCSS } from '../../scripts/lib-franklin.js';
 
-const embedYoutube = (url) => {
+const embedYoutube = (url, isLite) => {
   const usp = new URLSearchParams(url.search);
   let suffix = '';
   let vid = usp.get('v');
@@ -29,10 +26,25 @@ const embedYoutube = (url) => {
   if (url.origin.includes('youtu.be')) {
     [, vid] = url.pathname.split('/');
   }
-  const embedHTML = `<div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;">
-      <iframe src="https://www.youtube.com${vid ? `/embed/${vid}?rel=0&v=${vid}${suffix}` : embed}" style="border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;" 
-      allow="autoplay; fullscreen; picture-in-picture; encrypted-media; accelerometer; gyroscope; picture-in-picture" scrolling="no" title="Content from Youtube" loading="lazy"></iframe>
-    </div>`;
+
+  let embedHTML;
+
+  if (isLite) {
+    const embedSplit = embed.split('/');
+    embedHTML = `
+      <lite-youtube videoid=${vid || embedSplit[embedSplit.length - 1]}>
+        <a href="https://www.youtube.com${vid ? `/embed/${vid}?rel=0&v=${vid}${suffix}` : embed}" class="lty-playbtn" title="Play Video">
+      </a>
+      </lite-youtube>`;
+    loadCSS(`${window.hlx.codeBasePath}/blocks/embed/lite-yt-embed.css`);
+    loadScript(`${window.hlx.codeBasePath}/blocks/embed/lite-yt-embed.js`);
+  } else {
+    embedHTML = `<div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;">
+        <iframe src="https://www.youtube.com${vid ? `/embed/${vid}?rel=0&v=${vid}${suffix}` : embed}" style="border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;" 
+        allow="autoplay; fullscreen; picture-in-picture; encrypted-media; accelerometer; gyroscope; picture-in-picture" scrolling="no" title="Content from Youtube" loading="lazy"></iframe>
+      </div>`;
+  }
+
   return embedHTML;
 };
 
@@ -50,12 +62,11 @@ const loadEmbed = (block, grandChilds, link) => {
 
   const config = EMBEDS_CONFIG.find((e) => e.match.some((match) => link.includes(match)));
   const url = new URL(link);
+  const isLite = block.classList.contains('lite');
+
   if (config) {
-    block.innerHTML = config.embed(url);
+    block.innerHTML = config.embed(url, isLite);
     block.classList = `block embed embed-${config.match[0]}`;
-  } else {
-    block.innerHTML = getDefaultEmbed(url);
-    block.classList = 'block embed';
   }
   block.classList.add('embed-is-loaded');
 
