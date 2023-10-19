5680726e419708e436b7fec722828a6ccdec07ad
hparwani2 <49381235+hparwani2@users.noreply.github.com>
2023-10-19 21:28:23 +0530

    Header mobile (#147)
    * adding top and middle nav

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -1,13 +1,31 @@
 import { fetchPlaceholders, getMetadata } from '../../scripts/lib-franklin.js';
+import buildNavTree from './nav-tree-utils.js';
 import
 {
   getLanguage,
-  getLanguangeSpecificPath,
   getSearchWidget,
-  getWindowSize,
   fetchIndex,
   decorateAnchors,
+  htmlToElement,
 } from '../../scripts/scripts.js';
+
+function decorateSocial(social) {
+  social.classList.add('social');
+  social.innerHTML = social.innerHTML.replace(/\[social\]/, '');
+  social.querySelectorAll(':scope>ul>li').forEach((li) => {
+    const a = li.querySelector('a');
+    a.setAttribute('target', '_blank');
+    if (a.innerHTML.includes('linkedin')) {
+      a.setAttribute('aria-label', 'LinkedIn');
+    } else if (a.innerHTML.includes('twitter')) {
+      a.setAttribute('aria-label', 'Twitter');
+    } else if (a.innerHTML.includes('facebook')) {
+      a.setAttribute('aria-label', 'Facebook');
+    } else if (a.innerHTML.includes('youtube')) {
+      a.setAttribute('aria-label', 'YouTube');
+    }
+  });
+}
 
 async function decorateWebsitePicker(websitePicker) {
   websitePicker.classList.add('picker');
@@ -26,6 +44,55 @@ async function decorateWebsitePicker(websitePicker) {
   if (websitePicker.querySelectorAll(':scope>ul>li').length === 0 && websitePicker.querySelector('ul')) {
     websitePicker.querySelector('ul').remove();
   }
+}
+
+/* Decorate the other items - which is the items pulled from top nav */
+function decorateOtherItems(otherItemsEl) {
+  otherItemsEl.classList.add('other-items');
+
+  /* Pull items from the top nav */
+  document.querySelector('nav.nav-top').querySelectorAll(':scope>ul>li').forEach((li) => {
+    otherItemsEl.appendChild(li.cloneNode(true));
+  });
+
+  /* Make a website picker for mobile */
+  const websitePicker = document.createElement('li');
+  websitePicker.classList.add('mobile-website-picker');
+  const websitePickerUl = document.createElement('ul');
+  const title = otherItemsEl.querySelector('.website-picker').querySelector(':scope>a');
+  websitePicker.appendChild(title);
+  websitePicker.appendChild(websitePickerUl);
+  otherItemsEl.querySelector('.website-picker').querySelectorAll(':scope>ul>li').forEach((li) => {
+    websitePickerUl.appendChild(li.cloneNode(true));
+  });
+
+  websitePicker.querySelectorAll(':scope>ul>li').forEach((li) => {
+    li.classList.add('mobile-website-picker-item');
+    li.classList.remove('website-picker-item');
+    li.classList.remove('picker-item');
+  });
+
+  otherItemsEl.querySelector('.website-picker').replaceWith(websitePicker);
+
+  /* Make a lang picker for mobile */
+  const langPicker = document.createElement('li');
+  langPicker.classList.add('mobile-lang-picker');
+  const langPickerUl = document.createElement('ul');
+  langPicker.appendChild(langPickerUl);
+  otherItemsEl.querySelector('.lang-picker').querySelectorAll(':scope>ul>li').forEach((li) => {
+    langPickerUl.appendChild(li.cloneNode(true));
+  });
+
+  langPicker.querySelectorAll(':scope>ul>li').forEach((li) => {
+    li.classList.add('mobile-lang-picker-item');
+    li.classList.remove('lang-picker-item');
+    li.classList.remove('picker-item');
+  });
+
+  otherItemsEl.querySelector('.lang-picker').replaceWith(langPicker);
+
+  /* Move the social icons to the bottom */
+  otherItemsEl.appendChild(otherItemsEl.querySelector('.social'));
 }
 
 async function decorateLangPicker(langPicker) {
@@ -77,112 +144,14 @@ async function decorateLangPicker(langPicker) {
   }
 }
 
-/* Add levels to the menu items */
-function addLevels(root) {
-  const ulElements = root.querySelectorAll('ul');
-
-  ulElements.forEach((ul) => {
-    let level = 1;
-    let currentElement = ul;
-
-    while (currentElement.parentElement) {
-      if (currentElement.parentElement.tagName === 'UL') {
-        level += 1;
-      }
-
-      currentElement = currentElement.parentElement;
-    }
-
-    ul.classList.add(`menu-level-${level}`);
-    ul.querySelectorAll(':scope>li').forEach((li) => {
-      li.classList.add(`menu-level-${level}-item`);
-    });
-  });
-}
-
-function buildDropDownMenu(l1menuItem, placeholders) {
-  if (l1menuItem.querySelectorAll('ul').length === 0) return;
-  const dropDownMenu = document.createElement('div');
-  dropDownMenu.classList.add('dropdown-menu');
-  const dropDownHeader = document.createElement('div');
-  dropDownHeader.classList.add('dropdown-menu-header');
-  dropDownHeader.innerHTML = `
-    <h2>${l1menuItem.querySelector('a').innerHTML}</h2>
-    <a href="${getLanguangeSpecificPath('/global-network')}">
-      ${placeholders['learn-about-regional-availability']}
-      <span class="icon icon-ang-white"></span>
-    </a>
-  `;
-  dropDownMenu.appendChild(dropDownHeader);
-  dropDownMenu.appendChild(l1menuItem.querySelector('ul'));
-  l1menuItem.appendChild(dropDownMenu);
-
-  // Create an intersection observer instance for the dropdown menu
-  // Bring the backdrop in and out of view depending on the dropdown menu's visibility
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        document.querySelector('.backdrop').classList.add('visible');
-      } else {
-        document.querySelector('.backdrop').classList.remove('visible');
-      }
-    });
-  });
-
-  observer.observe(dropDownMenu);
-
-  l1menuItem.addEventListener('click', (evt) => {
-    if (getWindowSize().width < 1232) {
-      evt.preventDefault();
-      evt.stopPropagation();
-      const li = document.createElement('li');
-      li.classList.add('menu-level-2-item-header');
-      li.innerHTML = l1menuItem.querySelector('a').outerHTML;
-      l1menuItem.querySelector(':scope .menu-level-2').prepend(li);
-      l1menuItem.querySelector(':scope .menu-level-2').classList.toggle('open');
-      document.querySelector('.dropdown-menu').classList.toggle('open');
-      document.querySelector('.menu-back-btn').classList.toggle('visible');
-      document.querySelector('.mobile-icon').classList.toggle('visible');
-    }
-  });
-
-  dropDownMenu.addEventListener('click', (evt) => {
-    if (getWindowSize().width < 1232) {
-      if (evt.target.parentElement.classList.contains('menu-level-1-item')) {
-        const level2Menu = evt.target.parentElement.querySelector('ul');
-        if (level2Menu) {
-          evt.preventDefault();
-          const parent = level2Menu.closest('.menu-level-1-item');
-          const li = document.createElement('li');
-          li.classList.add('menu-level-2-item-header');
-          li.innerHTML = parent.querySelector('a').outerHTML;
-          level2Menu.prepend(li);
-          level2Menu.classList.toggle('open');
-        }
-      } else if (evt.target.parentElement.classList.contains('menu-level-2-item')) {
-        const level3Menu = evt.target.parentElement.querySelector('ul');
-        if (level3Menu) {
-          evt.preventDefault();
-          const parent = level3Menu.closest('.menu-level-2-item');
-          const li = document.createElement('li');
-          li.classList.add('menu-level-3-item-header');
-          li.innerHTML = parent.querySelector('a').outerHTML;
-          level3Menu.prepend(li);
-
-          level3Menu.classList.toggle('open');
-        }
-      }
-      evt.stopPropagation();
-    }
-  });
-}
-
 function decorateTopNav(nav) {
   nav.querySelectorAll(':scope>ul>li').forEach((li) => {
     if (li.textContent.includes('[languages]')) {
       decorateLangPicker(li);
     } else if (li.textContent.includes('[websites]')) {
       decorateWebsitePicker(li);
+    } else if (li.textContent.trim() === '[social]') {
+      decorateSocial(li);
     }
   });
 }
@@ -192,85 +161,74 @@ function decorateMiddleNav(nav) {
   a.setAttribute('aria-label', 'Sunstar Home');
 }
 
-/* Decorate the other items - which is the items pulled from top nav */
-function decorateOtherItems(otherItemsEl) {
-  otherItemsEl.classList.add('other-items');
-
-  /* Pull items from the top nav */
-  document.querySelector('nav.nav-top').querySelectorAll(':scope>ul>li').forEach((li) => {
-    otherItemsEl.appendChild(li.cloneNode(true));
+function getNavbarToggler() {
+  const navbarToggl = htmlToElement(`<button class="navbar-toggler">
+  <span class="mobile-icon">
+    <i></i>
+    <i></i>
+    <i></i>
+    <i></i>
+  </span>
+  </button>`);
+  const widerScreenWidth = window.matchMedia('(min-width: 77rem)');
+  if (!widerScreenWidth.matches) {
+    navbarToggl.classList.add('visible');
+  }
+  navbarToggl.addEventListener('click', () => {
+    const navBottom = document.querySelector('.nav-bottom');
+    const header = document.querySelector('header');
+    const { body } = document;
+    if (navBottom.classList.contains('open')) {
+      navBottom.classList.remove('open');
+      header.classList.remove('menu-open');
+      body.classList.remove('fixed');
+    } else {
+      navBottom.classList.add('open');
+      header.classList.add('menu-open');
+      body.classList.add('fixed');
+    }
   });
-
-  /* Make a lang picker for mobile */
-  const langPicker = document.createElement('li');
-  langPicker.classList.add('mobile-lang-picker');
-  const langPickerUl = document.createElement('ul');
-  langPicker.appendChild(langPickerUl);
-  otherItemsEl.querySelector('.lang-picker').querySelectorAll(':scope>ul>li').forEach((li) => {
-    langPickerUl.appendChild(li.cloneNode(true));
-  });
-
-  langPicker.querySelectorAll(':scope>ul>li').forEach((li) => {
-    li.classList.add('mobile-lang-picker-item');
-    li.classList.remove('lang-picker-item');
-  });
-
-  otherItemsEl.querySelector('.lang-picker').replaceWith(langPicker);
+  return navbarToggl;
 }
 
-function decorateBottomNav(nav, placeholders) {
-  addLevels(nav);
-  nav.querySelectorAll(':scope .menu-level-1-item').forEach((li) => {
-    buildDropDownMenu(li, placeholders);
-  });
+function attachWindowResizeListeners(nav) {
+  const header = document.querySelector('header');
+  const { body } = document;
+  const widerScreenWidth = window.matchMedia('(min-width: 77rem)');
+  widerScreenWidth.addEventListener('change', (event) => {
+    const toggler = nav.querySelector('.navbar-toggler');
+    if (event.matches) {
+      if (nav.classList.contains('open')) {
+        nav.classList.remove('open');
+        header.classList.remove('menu-open');
+        body.classList.remove('fixed');
+      }
+      if (toggler.classList.contains('visible')) {
+        toggler.classList.remove('visible');
+      }
+      const visibleMegaDrop = nav.querySelector('.mega-dropdown.visible');
+      if (visibleMegaDrop) {
+        visibleMegaDrop.classList.remove('visible');
+      }
+      const backButton = nav.querySelector('.menu-back-btn');
+      if (backButton) {
+        backButton.remove();
+      }
+    } else {
+      toggler.classList.add('visible');
+    }
+  }, true);
+}
 
-  /* Add the other items to bottom nav */
+function decorateBottomNav(nav, placeholders, navTreeJson) {
+  const navTree = buildNavTree(navTreeJson);
+  nav.append(getNavbarToggler());
+  nav.append(navTree);
+  nav.append(getSearchWidget(placeholders));
   const otherItemsEl = document.createElement('li');
   decorateOtherItems(otherItemsEl);
   nav.querySelector(':scope .menu-level-1').append(otherItemsEl);
-
-  const hamburger = document.createElement('span');
-  hamburger.classList.add('mobile-icon');
-  hamburger.classList.add('visible');
-  hamburger.innerHTML = Array.from({ length: 4 }, () => '<i></i>').join(' ');
-  nav.prepend(hamburger);
-
-  hamburger.addEventListener('click', () => {
-    nav.classList.toggle('open');
-    nav.querySelector(':scope .menu-level-1').classList.toggle('open');
-    document.body.classList.toggle('no-scroll');
-  });
-
-  const menuBackBtn = document.createElement('div');
-  menuBackBtn.classList.add('menu-back-btn');
-  menuBackBtn.innerHTML = `<span class="icon icon-angle-left"></span><a>${placeholders['back-to-menu']}</a>`;
-  nav.prepend(menuBackBtn);
-  nav.append(getSearchWidget(placeholders));
-
-  menuBackBtn.addEventListener('click', () => {
-    const level1Open = nav.querySelector(':scope .menu-level-1.open');
-    const level2Open = nav.querySelector(':scope .menu-level-2.open');
-    const level3Open = nav.querySelector(':scope .menu-level-3.open');
-
-    if (level3Open) {
-      level3Open.querySelector(':scope .menu-level-3-item-header').remove();
-      level3Open.classList.remove('open');
-      return;
-    }
-
-    if (level2Open) {
-      level2Open.querySelector(':scope .menu-level-2-item-header').remove();
-      level2Open.closest('.dropdown-menu').classList.remove('open');
-      level2Open.classList.remove('open');
-      menuBackBtn.classList.remove('visible');
-      hamburger.classList.add('visible');
-      return;
-    }
-
-    if (level1Open) {
-      level1Open.classList.remove('open');
-    }
-  });
+  attachWindowResizeListeners(nav);
 }
 
 const navDecorators = { 'nav-top': decorateTopNav, 'nav-middle': decorateMiddleNav, 'nav-bottom': decorateBottomNav };
@@ -283,22 +241,26 @@ export default async function decorate(block) {
   const navMeta = getMetadata('nav');
   const navPath = navMeta || (getLanguage() === 'en' ? '/_drafts/himanshu/nav' : `/${getLanguage()}/nav`);
   const resp = await fetch(`${navPath}.plain.html`);
-
+  const navTreeResp = await fetch(`/nav-tree.json?sheet=${getLanguage()}`);
+  const navTreeJson = await navTreeResp.json();
   if (resp.ok) {
     const placeholders = await fetchPlaceholders(getLanguage());
     block.innerHTML = '';
     const html = await resp.text();
     const fetchedNav = document.createElement('div');
     fetchedNav.innerHTML = html;
-    const navClasses = ['nav-top', 'nav-middle', 'nav-bottom'];
+    const navClasses = ['nav-top', 'nav-middle'];
     navClasses.forEach((navClass, idx) => {
       const nav = document.createElement('nav');
       nav.classList.add(navClass);
       nav.innerHTML = fetchedNav.querySelectorAll(':scope>div')[idx].innerHTML;
       navDecorators[navClass](nav, placeholders);
-
       block.appendChild(nav);
     });
+    const nav = document.createElement('nav');
+    nav.classList.add('nav-bottom');
+    navDecorators['nav-bottom'](nav, placeholders, navTreeJson);
+    block.appendChild(nav);
 
     window.addEventListener('scroll', () => {
       if (document.documentElement.scrollTop > document.querySelector('nav.nav-top').offsetHeight + document.querySelector('nav.nav-middle').offsetHeight) {
