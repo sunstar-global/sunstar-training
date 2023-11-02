8bb5c4abcd8db264ef8f4ba253104e8478a7d807
RitwikSrivastava <45959816+RitwikSrivastava@users.noreply.github.com>
2023-11-02 12:09:56 +0530

    Enhance hero-horizontal-tabs to accomodate 0 tabs (#255)
    * Update hero-horizontal-tabs.js
    
    * Update hero-horizontal-tabs.js
    
    * Update hero-horizontal-tabs.js
    
    * Update hero-horizontal-tabs.js

--- blocks/hero-horizontal-tabs/hero-horizontal-tabs.js
+++ blocks/hero-horizontal-tabs/hero-horizontal-tabs.js
@@ -31,14 +31,22 @@ export default function decorate(block) {
   const image = getImage(block);
   const text = getText(block);
   const tabs = createTabs(block, text);
-
+  if (tabs) {
   // move the tab riders in front
-  const wrapper = block.parentElement;
-  const container = wrapper.parentElement;
-  container.insertBefore(wrapper, container.firstElementChild);
+    const wrapper = block.parentElement;
+    const container = wrapper.parentElement;
+    container.insertBefore(wrapper, container.firstElementChild);
 
-  addTabs(tabs, block);
-
+    addTabs(tabs, block);
+  } else {
+    block.firstElementChild.remove();
+    const divs = document.querySelectorAll('div'); // Select all div elements
+    divs.forEach((div) => {
+      if (div.textContent.trim() === 'Contents') {
+        div.remove(); // Remove the div with the exact text content "Contents"
+      }
+    });
+  }
   if (image) {
     block.append(image);
   } else {
