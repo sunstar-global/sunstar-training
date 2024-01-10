5a06dfab3b9b40a135338114f41665b34ffbf1e1
Dragos Cristian Bute <dbute@Dragoss-MacBook-Pro.local>
2024-01-10 12:22:08 +0200

    fixed captions no longer working

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -108,10 +108,28 @@ function buildImageCollageForPicture(picture, caption, buildBlockFunction) {
   const captionText = caption.textContent;
   const captionP = document.createElement('p');
   captionP.innerHTML = captionText;
+  captionP.classList.add('image-caption');
   caption.remove();
   const newBlock = buildBlockFunction('image-collage', { elems: [picture, captionP] });
   newBlock.classList.add('boxy-col-1');
   return newBlock;
+}
+
+function formatAutoblockedImageCaptionsForColumns(block, enclosingDiv) {
+  const picture = block.querySelector('picture');
+  const caption = block.querySelector('p');
+  const blockClassList = block.classList;
+  const columnDiv = document.createElement('div');
+
+  if (enclosingDiv.parentElement?.classList?.contains('columns') || enclosingDiv.parentElement?.parentElement?.classList?.contains('columns')) {
+    columnDiv.classList = blockClassList;
+    columnDiv.classList.add('img-col');
+    columnDiv.appendChild(picture);
+    columnDiv.appendChild(caption);
+
+    enclosingDiv.classList.add('img-col-wrapper');
+    enclosingDiv.replaceChild(columnDiv, block);
+  }
 }
 
 function buildImageWithCaptionForPicture(parentP, picture, buildBlockFunction) {
@@ -143,6 +161,7 @@ function buildImageWithCaptionForPicture(parentP, picture, buildBlockFunction) {
         }
         // insert the new block at the position the old image was at
         enclosingDiv.replaceChild(newBlock, parentP);
+        formatAutoblockedImageCaptionsForColumns(newBlock, enclosingDiv);
         return;
       }
 
@@ -160,6 +179,7 @@ function buildImageWithCaptionForPicture(parentP, picture, buildBlockFunction) {
         const newBlock = buildImageCollageForPicture(picture, cp, buildBlockFunction);
         newBlock.classList.add('autoblocked');
         enclosingDiv.replaceChild(newBlock, parentP);
+        formatAutoblockedImageCaptionsForColumns(newBlock, enclosingDiv);
         return;
       }
     }
