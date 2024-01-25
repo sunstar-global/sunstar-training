57d154c765a2f859ecb89d314da9f415719382d3
Dejan Eric <dejan.eric@outlook.com>
2024-01-25 10:20:55 +0100

    Revert "Add GTM"

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -22,56 +22,7 @@ window.addEventListener('consentmanager', () => {
   }
 });
 
-/**
- * Google Tag Manager
-* */
-async function loadGTM() {
-  const scriptTag = document.createElement('script');
-  scriptTag.innerHTML = `
-        (function (w, d, s, l, i) {
-        w[l] = w[l] || [];
-        w[l].push({
-            'gtm.start':
-                new Date().getTime(), event: 'gtm.js'
-        });
-        var f = d.getElementsByTagName(s)[0],
-            j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
-        j.async = true;
-        j.src =
-            'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-        f.parentNode.insertBefore(j, f);
-        }(window, document, 'script', 'dataLayer', 'GTM-TZS45SW'));
-    `;
-  document.head.prepend(scriptTag);
-}
-
-function loadGAScript(url, callback) {
-  const head = document.querySelector('head');
-  let script = head.querySelector(`script[src="${url}"]`);
-  if (!script) {
-    script = document.createElement('script');
-    script.src = url;
-    script.async = true;
-    head.append(script);
-    script.onload = callback;
-    return script;
-  }
-  return script;
-}
-
-async function loadGA() {
-  const gaId = 'UA-150288508-1';
-  loadGAScript(`https://www.googletagmanager.com/gtag/js?id=${gaId}`, () => {
-  // eslint-disable-next-line
-    window.dataLayer = window.dataLayer || []; function gtag() { dataLayer.push(arguments); } gtag('js', new Date()); gtag('config', gaId); ga('send', 'pageview'); ga('create', gaId, 'auto');
-  });
-}
-
 if (!isInternalPage()) {
   await loadConsentManager();
   await loadAdobeLaunch();
-  if (getEnvType() === 'live') {
-    await loadGTM();
-    await loadGA();
-  }
 }
