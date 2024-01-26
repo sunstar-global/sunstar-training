b1c787d9904d461fe30cbe692e68922d6722e563
Dejan Eric <dejan.eric@outlook.com>
2024-01-26 08:25:56 +0000

    Jiang Code

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,11 +1,8 @@
-// eslint-disable-next-line import/no-cycle
 import { sampleRUM, isInternalPage } from './lib-franklin.js';
 // eslint-disable-next-line import/no-cycle
 import { getEnvType, loadConsentManager, loadScript } from './scripts.js';
-
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
-
 async function loadAdobeLaunch() {
   const adobeotmSrc = {
     dev: 'https://assets.adobedtm.com/467469cdd595/f9651373cafd/launch-a46d93f0c752-development.min.js',
@@ -14,7 +11,6 @@ async function loadAdobeLaunch() {
   };
   await loadScript(adobeotmSrc[getEnvType()]);
 }
-
 window.addEventListener('consentmanager', () => {
   if (window.uc) {
     // unblock adobe launch
@@ -23,28 +19,8 @@ window.addEventListener('consentmanager', () => {
 });
 
 /**
- * Google Tag Manager
+ * GA Tag
 * */
-async function loadGTM() {
-  const scriptTag = document.createElement('script');
-  scriptTag.innerHTML = `
-        (function (w, d, s, l, i) {
-        w[l] = w[l] || [];
-        w[l].push({
-            'gtm.start':
-                new Date().getTime(), event: 'gtm.js'
-        });
-        var f = d.getElementsByTagName(s)[0],
-            j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
-        j.async = true;
-        j.src =
-            'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-        f.parentNode.insertBefore(j, f);
-        }(window, document, 'script', 'dataLayer', 'GTM-TZS45SW'));
-    `;
-  document.head.prepend(scriptTag);
-}
-
 function loadGAScript(url, callback) {
   const head = document.querySelector('head');
   let script = head.querySelector(`script[src="${url}"]`);
@@ -60,7 +36,7 @@ function loadGAScript(url, callback) {
 }
 
 async function loadGA() {
-  const gaId = 'UA-150288508-1';
+  const gaId = 'G-EBP844FZRJ';
   loadGAScript(`https://www.googletagmanager.com/gtag/js?id=${gaId}`, () => {
   // eslint-disable-next-line
     window.dataLayer = window.dataLayer || []; function gtag() { dataLayer.push(arguments); } gtag('js', new Date()); gtag('config', gaId); ga('send', 'pageview'); ga('create', gaId, 'auto');
@@ -71,7 +47,6 @@ if (!isInternalPage()) {
   await loadConsentManager();
   await loadAdobeLaunch();
   if (getEnvType() === 'live') {
-    await loadGTM();
     await loadGA();
   }
 }
